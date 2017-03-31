d25acee6d4d1a91cbe153bffafd416c18dfce3b7
Kean Drake <kean.drake@banno.com>
2017-03-31 12:31:57 -0500

    Working on a function that takes the default config and adds/replaces values with the custom config's

--- src/services/chart-config.service.js
+++ src/services/chart-config.service.js
@@ -4,9 +4,9 @@ const chartConfigService = {
     for (let datasetIndex in dataConfig.datasets) {
       for (let property of properties) {
         if (!dataConfig.datasets[datasetIndex].hasOwnProperty(property)) {
-          dataConfig.datasets[datasetIndex][property] = defaultConfig.data.datasets[datasetIndex][property] ?
-                                                        defaultConfig.data.datasets[datasetIndex][property] :
-                                                        defaultConfig.data.datasets[0][property];
+          dataConfig.datasets[datasetIndex][property] = defaultConfig.line.data.datasets[datasetIndex][property] ?
+                                                        defaultConfig.line.data.datasets[datasetIndex][property] :
+                                                        defaultConfig.line.data.datasets[0][property];
         } else {
           return;
         }
@@ -41,17 +41,17 @@ const chartConfigService = {
               _setPropertyToDefault(optionsConfig.scales.xAxes[i].gridLines, 'color', 'rgba(0,0,0,0)');
               _setPropertyToDefault(optionsConfig.scales.xAxes[i].gridLines, 'drawBorder', false);
             } else {
-              optionsConfig.scales.xAxes[i].gridLines = defaultConfig.options.scales.xAxes[0].gridLines;
+              optionsConfig.scales.xAxes[i].gridLines = defaultConfig.line.options.scales.xAxes[0].gridLines;
             }
 
             if (optionsConfig.scales.xAxes[i].ticks) {
               _setPropertyToDefault(optionsConfig.scales.xAxes[i].ticks, 'maxRotation', 0);
             } else {
-              optionsConfig.scales.xAxes[i].ticks = defaultConfig.options.scales.xAxes[i].ticks;
+              optionsConfig.scales.xAxes[i].ticks = defaultConfig.line.options.scales.xAxes[i].ticks;
             }
           }
         } else {
-          optionsConfig.scales.xAxes = defaultConfig.options.scales.xAxes;
+          optionsConfig.scales.xAxes = defaultConfig.line.options.scales.xAxes;
         }
 
         if (optionsConfig.scales.yAxes) {
@@ -59,26 +59,60 @@ const chartConfigService = {
             if (optionsConfig.scales.yAxes[i].gridLines) {
               _setPropertyToDefault(optionsConfig.scales.yAxes[i].gridLines, 'drawBorder', false);
             } else {
-              optionsConfig.scales.yAxes[i].gridLines = defaultConfig.options.scales.yAxes[0].gridLines;
+              optionsConfig.scales.yAxes[i].gridLines = defaultConfig.line.options.scales.yAxes[0].gridLines;
             }
 
             if (optionsConfig.scales.yAxes[i].ticks) {
               _setPropertyToDefault(optionsConfig.scales.yAxes[i].ticks, 'padding', 20);
             } else {
-              optionsConfig.scales.yAxes[i].ticks = defaultConfig.options.scales.yAxes[0].ticks;
+              optionsConfig.scales.yAxes[i].ticks = defaultConfig.line.options.scales.yAxes[0].ticks;
             }
           }
         } else {
-          optionsConfig.scales.yAxes = defaultConfig.options.scales.yAxes;
+          optionsConfig.scales.yAxes = defaultConfig.line.options.scales.yAxes;
         }
       } else {
-        optionsConfig.scales = defaultConfig.options.scales;
+        optionsConfig.scales = defaultConfig.line.options.scales;
       }
     } else {
-      optionsConfig = defaultConfig.options;
+      optionsConfig = defaultConfig.line.options;
     }
 
     return optionsConfig;
+  },
+
+  _setDefaults(customChartOptions) {
+    let defaultChartOptions = defaultConfig[customChartOptions.type];
+    let returnChartOptions = {};
+    let keys = [];
+
+    _lookThroughObject = object => {
+      for (let key in object) {
+        if (object[key] instanceof Object && !(key === 'datasets' || key === 'labels')) {
+          keys.push(key);
+          returnChartOptions[key] = {};
+          _lookThroughObject(object[key]);
+          keys.pop();
+        } else {
+          let defaultPath = 'defaultChartOptions';
+          for (let i = 0; i < keys.length; i++) {
+            // Needs to be figured out
+            //
+            // console.log('loop ' + i + ': ');
+            // console.log(defaultPath);
+            // console.log(keys[i]);
+            // console.log(eval(defaultPath)[keys[i]]);
+            // if (eval(defaultPath).hasOwnProperty([keys[i]])) {
+            //   defaultPath = defaultPath + '[' + keys[i] + ']';
+            // } else {
+            //   eval(defaultPath)[keys[i]] = object[key];
+            // }
+          }
+        }
+      }
+    }
+
+    _lookThroughObject(customChartOptions);
   }
 }
 
